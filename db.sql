1a27010bcb86d32b92f2ea556c09bfff93afec3c
谢致邦 (XIE Zhibang) <Yeking@Red54.com>
2017-02-04 17:35:19 +0800

    1.1.0

--- db.sql
+++ db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Feb 02, 2017 at 07:39 AM
+-- Generation Time: Feb 04, 2017 at 09:25 AM
 -- Server version: 10.1.21-MariaDB
 -- PHP Version: 7.1.1
 
@@ -23,31 +23,6 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `file`
---
-
-CREATE TABLE `file` (
-  `id` bigint(20) NOT NULL,
-  `infoid` bigint(20) NOT NULL,
-  `path` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `length` bigint(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `freq`
---
-
-CREATE TABLE `freq` (
-  `id` bigint(20) NOT NULL,
-  `infoid` bigint(20) NOT NULL,
-  `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `info`
 --
 
@@ -55,28 +30,17 @@ CREATE TABLE `info` (
   `id` bigint(20) NOT NULL,
   `infohash` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
   `name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `file` bigint(20) NOT NULL,
+  `filenum` bigint(20) NOT NULL,
   `length` bigint(20) NOT NULL,
   `freq` bigint(20) NOT NULL DEFAULT '1',
   `createtime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updatetime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+  `updatetime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `files` longtext COLLATE utf8mb4_unicode_ci
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Indexes for dumped tables
 --
-
---
--- Indexes for table `file`
---
-ALTER TABLE `file`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `freq`
---
-ALTER TABLE `freq`
-  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `info`
@@ -89,16 +53,6 @@ ALTER TABLE `info`
 -- AUTO_INCREMENT for dumped tables
 --
 
---
--- AUTO_INCREMENT for table `file`
---
-ALTER TABLE `file`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `freq`
---
-ALTER TABLE `freq`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `info`
 --
--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 
 const P2PSpider = require('p2pspider')
 const Client = require('mariasql')
+const iconv = require('iconv-lite')
+const jschardet = require('jschardet')
 
 const c = new Client({
 	host: '127.0.0.1',
@@ -12,16 +14,18 @@ const c = new Client({
 })
 
 const infosel = c.prepare('SELECT id FROM info WHERE infohash = ?')
-const infoins = c.prepare('INSERT INTO info (infohash, name, file, length) VALUES (?, ?, ?, ?)')
+const infoins = c.prepare('INSERT INTO info (infohash, name, filenum, length, files) VALUES (?, ?, ?, ?, ?)')
 const infoupd = c.prepare('UPDATE info SET freq = freq + 1 WHERE id = ?')
-const freqins = c.prepare('INSERT INTO freq (infoid) VALUES (?)')
-const fileins = c.prepare('INSERT INTO file (infoid, path, length) VALUES (?, ?, ?)')
 
 const p2p = P2PSpider({
 	nodesMaxSize: 200,   // be careful
 	maxConnections: 400, // be careful
 	timeout: 5000
 })
+
+function decode(buf) {
+	return iconv.decode(buf, jschardet.detect(buf).encoding)
+}
 
 p2p.ignore((infohash, rinfo, callback) => {
 	let exist = false
@@ -30,7 +34,6 @@ p2p.ignore((infohash, rinfo, callback) => {
 		if (res[0]) {
 			exist = true
 			c.query(infoupd([res[0].id]), (e, res) => { if (e) throw e })
-			c.query(freqins([res[0].id]), (e, res) => { if (e) throw e })
 		}
 	})
 	callback(exist)
@@ -38,31 +41,30 @@ p2p.ignore((infohash, rinfo, callback) => {
 })
 
 p2p.on('metadata', metadata => {
+	let filenum = 0
+	let length = 0
+	let files = null
+	const name = metadata.info['name.utf-8'] || decode(metadata.info.name)
 	if (metadata.info.files) {
-		var file = metadata.info.files.length
-		var length = metadata.info.files.reduce((pre, cur) => pre + cur.length, 0)
-	} else {
-		var file = 0
-		var length = metadata.info.length
-	}
-	c.query(infoins([metadata.infohash, metadata.info.name.toString(), file, length]), (e, res) => {
+		filenum = metadata.info.files.length
+		let path = null
+		files = metadata.info.files.reduce((pre, cur) => {
+			length += cur.length
+			path = cur['path.utf-8'] || decode(cur.path) + '//' + cur.length
+			if (pre) return pre + '///' + path
+			else return path
+		}, null)
+	} else length = metadata.info.length
+	c.query(infoins([metadata.infohash, name, filenum, length, files]), (e, res) => {
 		if (e) {
 			if (1062 == e.code) {
 				c.query(infosel([metadata.infohash]), (e, res) => {
 					if (e) throw e
-					if (res[0]) {
-						c.query(infoupd([res[0].id]), (e, res) => { if (e) throw e })
-						c.query(freqins([res[0].id]), (e, res) => { if (e) throw e })
-					}
+					if (res[0]) c.query(infoupd([res[0].id]), (e, res) => { if (e) throw e })
 				})
 				return
 			} else throw e
 		}
-		c.query(freqins([res.info.insertId]), (e, res) => { if (e) throw e })
-		if (metadata.info.files)
-			metadata.info.files.forEach((f) => {
-				c.query(fileins([res.info.insertId, f.path.toString(), f.length]), (e, res) => { if (e) throw e })
-			})
 	})
 	console.log('Add: ' + metadata.infohash)
 })
